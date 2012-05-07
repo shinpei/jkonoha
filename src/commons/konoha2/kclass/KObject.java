968d18c7ecff84edb734f285436e8db655e3f9b6
岡本 悠希 <okachin@okamoto-yuuki-no-MacBook.local>
2012-05-07 21:08:41 +0900

    add KObject

---
+++ src/commons/konoha2/kclass/KObject.java
@@ -0,0 +1,12 @@
+package commons.konoha2.kclass;
+
+import commons.konoha2.*;
+
+public class KObject {
+	public KObjectHeader h;
+	
+	/* union */
+	// TODO
+	// const struct _kObject fields[4];
+	// uintptr_t ndata[4];
+}
