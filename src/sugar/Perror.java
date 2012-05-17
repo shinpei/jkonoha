1c2b1d2de6eba5e6f935638c9cb6f2df923c3ebd
JosephLee <leejonggun@gmail.com>
2012-05-17 14:57:12 +0900

    Add Perror.java

--- src/sugar/Perror.java
+++ src/sugar/Perror.java
@@ -27,6 +27,40 @@ public class Perror {
 		return "(unknown)";
 	}
 	
+	public static int vperrorf(CTX ctx, int pe, int uline, int lpos, String fmt, K_Object... ap/*TODO*/) {//joseph: vperrorf in original konoha2 (src/sugar/perror.h)
+		String msg = T_emsg(ctx, pe);
+		int errref = -1;
+		if(msg != null) {
+			CtxSugar base = new CtxSugar();//ctx.KArrayList<KModLocal>;
+			Kwb wb;//TODO
+			/*typedef struct kwb_t { //in /include/konoha2/konoha2.h
+				karray_t *m;
+				size_t pos;
+			} kwb_t;*/
+			Kwb_init(base.cwb, wb);
+			if(uline > 0) {
+				String file = T_file(uline);//TODO T_file
+				kwb_printf(msg + KLib.shortname(file) + uline);// shortname is in KLib.java
+			}
+			else {
+				kwb_printf(wb, msg);
+			}
+			kwb_vprintf(wb, fmt, ap);
+			msg = kwb_top(wb, 1);
+			//TODO new_kSting, kArray_size, kArray_add
+			K_String emsg = new K_String(msg);
+			errref = base.errors.size();
+			base.errors.add(emsg);//TODO
+			if(pe == ERR_ || pe == CRIT_) {//TDOO ERR, CRIT_ enum in (/include/konoha2/konoha2.h)
+				base.errCount ++;
+			}
+			//kreport(pe, (String)emsg.h.ct.unbox(ctx, (K_Object)emsg));//TODO
+			//kreport(pe, (String)O_ct(emsg).unbox(ctx, (K_Object)emsg));//TODO O_ct(o) ((o)->h.ct)
+			kreport(pe, S_text(emsg));//TODO S_text(s) ((const char*) (O_ct(s)->unbox(_ctx, (kObject*)s)))
+		}									//			(String)O_ct(s).unbox(ctx, (K_Object)s)
+		return errref;
+	}
+	
 	public static int sugar_p(CTX ctx, int pe, int uline, int lpos, Object... ap) {
 		int errref = vperrorf(ctx, pe, uline, lpos, ap); //TODO vperrorf is not exist
 		return errref;
@@ -36,6 +70,11 @@ public class Perror {
 		vperrorf(ctx, pe, tk.uline, tk.lpos, ap); //TODO vperrorf is not exist
 		// return K_NULLEXPR; TODO
 		return null;
+	}	
+	
+	public static int Kerrno(CTX ctx) {//joseph: Kerrno in original konoha2 (src/sugar/perror.h)
+		//K_Array<K_Object> i = new K_Array<K_Object>();
+		return ctx.modlocal.errors.size();//TODO
 	}
 	
 	public static K_String KStrerror(CTX ctx, int eno) {
